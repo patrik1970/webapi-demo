a24d3c9a8ba22d7368b2805c02ff5f0f52775062
Patrik Persson <patrik.persson10@tele2.se>
2021-02-08 23:22:22 +0100

    Uploading project

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleDemo;
+using System;
+using System.Net.Http;
+
+swaggerClient client = new("https://localhost:44373", new HttpClient());
+
+var records = await client.WeatherForecastAsync();
+
+foreach (var r in records) 
+{
+    Console.WriteLine($"{ r.Date }: { r.Summary }");
+}       
